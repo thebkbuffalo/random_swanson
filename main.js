a45a6acb52012dcd862eb1a20096ef82c02aacb1
Evan Berg <evan.m.berg@gmail.com>
2016-04-30 19:32:21 -0700

    added some images gotta get im randomly popping up

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
   console.log('linked up');
 
+  pics = ['pics/swanson1.png', 'pics/swanson2.jpg', 'pics/swanson3.jpg', 'pics/swanson4.jpg', 'pics/swanson5.jpg', 'pics/swanson6.jpg', 'pics/swanson7.jpg', 'pics/swanson8.jpg', 'pics/swanson9.png', 'pics/swanson10.jpg']
 
   $('#next').on('click', function(){
+    console.log(pics[Math.floor(Math.random() * pics.length)]);
     $.ajax({
       url: "http://ron-swanson-quotes.herokuapp.com/v2/quotes",
     }).done(function(data) {
       console.log(data[0]);
+      $('#pics').empty().
       $('#quote').empty().append(data[0]);
     });
   });
